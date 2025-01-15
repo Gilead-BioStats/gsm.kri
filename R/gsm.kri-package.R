07d65a9986529375c058fec610bdeb770dd1d279
= <laura.maxwell@atorusresearch.com>
2025-01-15 11:02:31 -0500

    add reporting functions, docs and dependencies

--- R/gsm.kri-package.R
+++ R/gsm.kri-package.R
@@ -1,6 +1,7 @@
 ## usethis namespace: start
 #' @import dplyr
 #' @import gsm
+#' @import gt
 #' @import purrr
 #' @import log4r
 #' @import tidyr
@@ -12,5 +13,20 @@
 #' @importFrom cli cli_alert_warning
 #' @importFrom cli cli_text
 #' @importFrom cli cli_warn
+#' @importFrom htmltools h1
+#' @importFrom htmltools h2
+#' @importFrom htmltools h3
+#' @importFrom htmltools h4
+#' @importFrom htmltools HTML
+#' @importFrom htmltools p
+#' @importFrom htmltools strong
+#' @importFrom htmltools tagList
+#' @importFrom htmltools tags
+#' @importFrom htmlwidgets createWidget
+#' @importFrom htmlwidgets prependContent
+#' @importFrom htmlwidgets shinyRenderWidget
+#' @importFrom htmlwidgets shinyWidgetOutput
+#' @importFrom jsonlite fromJSON
+#' @importFrom jsonlite toJSON
 ## usethis namespace: end
 NULL
