939205bb73e3be3ab0bd1effee70a12a430990a3
Laura Maxwell <laura.maxwell@atorusresearch.com>
2025-01-29 12:48:19 -0500

    update package deps

--- R/gsm.kri-package.R
+++ R/gsm.kri-package.R
@@ -4,18 +4,8 @@
 #' @import gsm
 #' @import gt
 #' @import purrr
-#' @import log4r
 #' @import tidyr
-#' @importFrom cli cli_abort
-#' @importFrom cli cli_alert
-#' @importFrom cli cli_alert_info
-#' @importFrom cli cli_alert_danger
-#' @importFrom cli cli_alert_success
-#' @importFrom cli cli_alert_warning
-#' @importFrom cli cli_text
-#' @importFrom cli cli_warn
 #' @importFrom glue glue
-#' @importFrom glue glue_collapse
 #' @importFrom htmltools h1
 #' @importFrom htmltools h2
 #' @importFrom htmltools h3
@@ -32,18 +22,14 @@
 #' @importFrom jsonlite fromJSON
 #' @importFrom jsonlite toJSON
 #' @importFrom lifecycle deprecated
+#' @importFrom log4r logger
+#' @importFrom log4r console_appender
 #' @importFrom rlang %||%
 #' @importFrom rlang .data
 #' @importFrom rlang check_installed
-#' @importFrom rlang expr
 #' @importFrom stats reorder
 #' @importFrom stats setNames
-#' @importFrom stringr str_detect
-#' @importFrom stringr str_extract
-#' @importFrom stringr str_replace
-#' @importFrom stringr word
 #' @importFrom tibble enframe
-#' @importFrom tibble rownames_to_column
 #' @importFrom tibble tibble
 ## usethis namespace: end
 NULL
