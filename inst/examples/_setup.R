17155738c4858a85e5ebf391b286930676bb61cf
Zelos Zhu <zelos.zhu@atorusresearch.com>
2025-03-06 13:34:49 -0800

    Update inst/examples/_setup.R
    Co-authored-by: Laura Maxwell <laura.maxwell@atorusresearch.com>

--- inst/examples/_setup.R
+++ inst/examples/_setup.R
@@ -1,5 +1,5 @@
 devtools::install_github('Gilead-BioStats/gsm@dev')
 devtools::install_github('Gilead-BioStats/gsm.mapping@dev')
-devtools::install_github('Gilead-BioStats/gsm.datam@fix-34')
+devtools::install_github('Gilead-BioStats/gsm.datasim@dev')
 
 # or gsm git checkout branch and devtools::load_all()
