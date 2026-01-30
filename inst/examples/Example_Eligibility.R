449226176131d40edbfcabdedb3a49d1b4fad21c
zdz2101 <zelos.zhu@atorusresearch.com>
2026-01-30 08:27:54 -0800

    switch back to at main

--- inst/examples/Example_Eligibility.R
+++ inst/examples/Example_Eligibility.R
@@ -6,10 +6,6 @@ library(gsm.kri)
 library(gsm.qtl)
 library(purrr)
 library(dplyr)
-devtools::load_all(".")
-devtools::load_all("../gsm.qtl") # fix-70
-devtools::load_all("../gsm.kri")
-pak::pak("Gilead-BioStats/gsm.qtl@fix-70")
 
 dfResults <- gsm.core::reportingResults_study %>%
   filter(MetricID %in% "Analysis_qtl0001") %>%
